d86aeaff05b2f0bd1b9ce4988999a9c42734606a
DevelopingPontus <ipont35@gmail.com>
2026-03-23 10:09:51 +0100

    ...

--- src/main/java/com/example/api/demo/generic/controllers/GenericController.java
+++ src/main/java/com/example/api/demo/generic/controllers/GenericController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 
 import com.example.api.demo.generic.interfaces.EntityInterface;
@@ -22,7 +21,6 @@ import jakarta.persistence.MappedSuperclass;
 import java.util.List;
 
 @MappedSuperclass
-@RequestMapping
 public abstract class GenericController<T extends EntityInterface, DTO> {
 
     protected final GenericService<T> service;
