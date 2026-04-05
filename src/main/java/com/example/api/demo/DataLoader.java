23cde49a50d6128922d3cf0aac7efc311b756c7f
DevelopingPontus <ipont35@gmail.com>
2026-04-05 14:57:27 +0200

    Added data loader

---
+++ src/main/java/com/example/api/demo/DataLoader.java
@@ -0,0 +1,48 @@
+package com.example.api.demo;
+
+import com.example.api.demo.author.AuthorRepository;
+import com.example.api.demo.book.dto.v1.BookReq1;
+import com.example.api.demo.book.service.BookService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookService bookService;
+
+    public DataLoader(AuthorRepository authorRepository, BookService bookService) {
+        this.authorRepository = authorRepository;
+        this.bookService = bookService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("--- Starting Database Seeding ---");
+
+        seedBooks();
+
+        System.out.println("--- Database Seeding Complete ---");
+    }
+
+    private void seedBooks() {
+
+        // Seed a book by George Orwell
+        List<BookReq1> books = new ArrayList<>();
+        BookReq1 book1 = new BookReq1("Animal Farm", "George Orwell", "12200", 1945);
+        books.add(book1);
+        BookReq1 book2 = new BookReq1("1984", "George Orwell", "12300", 1949);
+        books.add(book2);
+        BookReq1 book3 = new BookReq1("Brave New World", "Aldous Huxley", "12500", 1932);
+        books.add(book3);
+
+        bookService.save(books);
+
+    }
+}
